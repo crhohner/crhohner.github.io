eeaaacceb2f3086ea897af8783cccf37f7985779
crhohner <bowlsorice@gmail.com>
2024-08-09 00:15:38 -0500

    Add header links, hide test implementations

--- src/app/test/page.jsx
+++ src/app/test/page.jsx
@@ -1,9 +1,52 @@
+import Carousel from "../../components/carousel";
+
 export default function Page() {
+  const loremIpsum =
+    " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod" +
+    "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim" +
+    "veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea" +
+    "commodo consequat. Duis aute irure dolor in reprehenderit in voluptate" +
+    "velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint" +
+    "occaecat cupidatat non proident, sunt in culpa qui officia deserunt" +
+    " mollit anim id est laborum.";
+
+  const projects = [
+    {
+      name: "project one",
+      description:
+        "Seven seconds to the end. Time enough for you, perhaps... >:3",
+
+      path: "/gorgorgorl.png",
+    },
+    {
+      name: "project two",
+      description:
+        "sometimes i like to make myself peanut butter, honey, and banana sandwiches",
+      path: "/emoji.png",
+    },
+    {
+      name: "project three",
+      description: "hehehhe",
+      path: "/plush.png",
+    },
+  ];
   return (
     <>
-      {" "}
-      this is how next 14 routing works, always name file page.jsx and the
-      directory is the path you can find it at{" "}
+      <title>Home</title>
+
+      <Carousel projects={projects} />
+
+      <h>Welcome!</h>
+      <p>{loremIpsum}</p>
+      <div className="container">
+        <h>Skills</h>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </div>
     </>
   );
 }
--- src/components/header.js
+++ src/components/header.js
@@ -6,16 +6,30 @@ export default function Header() {
     <header className="header">
       <Link href="/">{"<Caroline Hohner/>"}</Link>
       <div className="links">
-        <Image src="./cuair.svg" height={32} width={32} alt="CuAir logo" />
-        <Image src="./github.svg" height={32} width={32} alt="GitHub logo" />
-        <Image
-          src="./linkedin.svg"
-          height={32}
-          width={32}
-          alt="LinkedIn logo"
-        />
+        <Link href="https://cuair.org">
+          <Image
+            href="https://cuair.org"
+            src="./cuair.svg"
+            height={32}
+            width={32}
+            alt="CuAir logo"
+          />
+        </Link>
+        <Link href="https://github.com/crhohner">
+          <Image src="./github.svg" height={32} width={32} alt="GitHub logo" />
+        </Link>
+        <Link href="https://www.linkedin.com/in/caroline-hohner-88875b212">
+          <Image
+            src="./linkedin.svg"
+            height={32}
+            width={32}
+            alt="LinkedIn logo"
+          />
+        </Link>
+        <Link href="https://drive.google.com/file/d/1agNvRl4oZd9B5EUGj52l7VIyQVqMFjkm/view?usp=sharing">
+          <Image src="./resume.svg" height={32} width={32} alt="resume icon" />
+        </Link>
       </div>
     </header>
   );
 }
-//add links
