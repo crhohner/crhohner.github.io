19c29ddb4d459d0e6c90783fe7e24e5e38f05fd4
crhohner <bowlsorice@gmail.com>
2024-08-10 18:35:36 -0500

    Add mobile support for projects carousel

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,11 +1,19 @@
 "use client";
-import { useState } from "react";
+import { isMobile } from "../utils";
+import { useState, useEffect } from "react";
+
 import Image from "next/image";
 /*import Rotatable from "./rotatable";
 import Preview from "./preview";*/
 
 export default function Carousel({ projects }) {
   const [index, setIndex] = useState(Math.floor(projects.length / 2));
+
+  const [isMobileDevice, setIsMobileDevice] = useState(false);
+
+  useEffect(() => {
+    setIsMobileDevice(isMobile());
+  }, []);
 
   function moveIndex(right) {
     var nextIndex = index;
@@ -30,26 +38,56 @@ export default function Carousel({ projects }) {
     />
   ));
 
+  const [touchStart, setTouchStart] = useState(null);
+  const [touchEnd, setTouchEnd] = useState(null);
+
+  const minSwipeDistance = 50;
+
+  const onTouchStart = (e) => {
+    setTouchEnd(null);
+    setTouchStart(e.targetTouches[0].clientX);
+  };
+
+  const onTouchMove = (e) => setTouchEnd(e.targetTouches[0].clientX);
+
+  const onTouchEnd = () => {
+    if (!touchStart || !touchEnd) return;
+    const distance = touchStart - touchEnd;
+    const isLeftSwipe = distance > minSwipeDistance;
+    const isRightSwipe = distance < -minSwipeDistance;
+    if (isLeftSwipe || isRightSwipe) moveIndex(isRightSwipe);
+  };
+
   return (
     <div style={{ paddingBottom: "4rem" }}>
       <div className="center">
         <div className="carousel">
-          <div className="cell">
-            {" "}
-            <button onClick={() => moveIndex(false)}>{" < "}</button>
-          </div>
+          {isMobileDevice || (
+            <div className="cell">
+              {" "}
+              <button onClick={() => moveIndex(false)}>{" < "}</button>
+            </div>
+          )}
 
           <div className="cell">
-            <div className="center">{models[index]}</div>
+            <div
+              className="center"
+              onTouchStart={onTouchStart}
+              onTouchMove={onTouchMove}
+              onTouchEnd={onTouchEnd}
+            >
+              {models[index]}
+            </div>
           </div>
 
-          <div className="cell">
-            {" "}
-            <button onClick={() => moveIndex(true)}>{" > "}</button>
-          </div>
+          {isMobileDevice || (
+            <div className="cell">
+              {" "}
+              <button onClick={() => moveIndex(true)}>{" > "}</button>
+            </div>
+          )}
         </div>
       </div>
-
       <div className="center">
         <div className="details">
           <h>{projects[index].name}</h>
